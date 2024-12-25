df22c386311a29b7de57f6ef261adb1101117214
vishalS013 <vishalsagarwins@gmail.com>
2024-12-25 15:49:06 +0530

    todo app

---
+++ src/TodoForm.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import { TextField, Button } from '@mui/material';
+
+function TodoForm({ addTodo }) {
+  const [title, setTitle] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (title.trim()) {
+      const newTodo = {
+        userId: Math.floor(Math.random() * 1000),
+        id: Math.floor(Math.random() * 2000), 
+        title,
+        completed: false,
+      };
+      addTodo(newTodo);
+      setTitle('');
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <TextField
+        label="New Todo"
+        variant="outlined"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        style={{ marginBottom: '20px' , marginTop:'5px', }}
+        sx={{ml:2}}
+      />
+      <Button variant="contained" color="primary" type="submit"  style={{marginLeft:"20px", marginTop:"15px"}}>
+        Add Todo
+      </Button>
+      
+    </form>
+  );
+}
+
+export default TodoForm;
---
+++ src/TodoItem.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { ListItem, ListItemText, IconButton, Checkbox } from '@mui/material';
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+
+function TodoItem({ todo, deleteTodo, setOpenEditDialog, setEditTodoId, setEditTodoTitle, setViewTodo, toggleComplete, handleDeleteClick }) {
+  const handleEditClick = () => {
+    setEditTodoId(todo.id)
+    setEditTodoTitle(todo.title)
+    setOpenEditDialog(true)
+  };
+
+  const handleViewClick = () => {
+    setViewTodo(todo.id)
+  };
+
+  const handleCompleteClick = () => {
+    toggleComplete(todo.id);
+  };
+
+  return (
+    <ListItem>
+      <Checkbox
+        checked={todo.completed}
+        onChange={handleCompleteClick}
+        color="primary"
+      />
+      <ListItemText 
+        primary={todo.title} 
+        style={{ textDecoration: todo.completed ? 'line-through' : 'none' }} 
+      />
+      <IconButton edge="end" onClick={handleEditClick} color="primary">
+        <EditIcon />
+      </IconButton>
+      <IconButton edge="end" onClick={() => handleDeleteClick(todo)} color="secondary">
+        <DeleteIcon />
+      </IconButton>
+      <IconButton edge="end" onClick={handleViewClick} color="default">
+        <VisibilityIcon />
+      </IconButton>
+    </ListItem>
+  );
+}
+
+export default TodoItem;
